598957a7e006befcedd6c951f4e120ee2cbd09cc
TMSKH <71212192+TMSKH@users.noreply.github.com>
2020-12-07 14:54:20 +0100

    Add files via upload

---
+++ VestbjergByggecenter/src/controlLayer/CustomerCtr.java
@@ -0,0 +1,24 @@
+package controlLayer;
+import modelLayer.*;
+
+/**
+ * This class is a part of the System for
+ * Vestbjerg Byggecenter. It acts as the 
+ * controller for interacting with the 
+ * customer objects in the system.
+ */
+
+public class CustomerCtr 
+{
+
+	public CustomerCtr()
+	{
+	}
+	
+	
+	public Customer getCustomer(int phone)
+	{
+		//TODO - update
+		return null;
+	}
+}
---
+++ VestbjergByggecenter/src/uiLayer/OrderMenu.java
@@ -0,0 +1,35 @@
+package uiLayer;
+
+import controlLayer.*;
+
+/**
+ * This class is a part of the System for
+ * Vestbjerg Byggecenter. It acts as the TUI 
+ * the user will interact with, whenever they manage
+ * anything regarding orders, offers and regular sales.
+ */
+
+import java.util.Scanner;
+
+
+public class OrderMenu 
+{
+	private Scanner input = new Scanner(System.in);
+	private OrderCtr orderCtr = new OrderCtr();
+	
+	
+	public OrderMenu()
+	{
+	}
+
+
+	public void start()
+	{
+		//TODO - update
+	}
+
+	private void createOffer()
+	{
+		//TODO - update
+	}
+}
