042b553b6b055b2454804acdf0a7b2b358a73598
Mette Juul Thorhauge Sørensen <mettekt94@gmail.com>
2020-12-11 15:06:34 +0100

    The createOrder() method has been finalised, and the last 5 methods have been modified, to get rid of redundancy and streamline the code.

--- VestbjergByggecenter/src/uiLayer/OrderMenu.java
+++ VestbjergByggecenter/src/uiLayer/OrderMenu.java
@@ -13,99 +13,79 @@ import modelLayer.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class OrderMenu
-{
+public class OrderMenu {
 	private Scanner input = new Scanner(System.in);
 	private OrderCtr orderCtr = new OrderCtr();
 
-	public OrderMenu()
-	{
+	public OrderMenu() {
 	}
 
-	public void start()
-	{
+	public void start() {
 		boolean running = true;
 
-		while (running)
-		{
+		while (running) {
 			System.out.println("* Order Menu *");
 			System.out.println(" (1) Create Offer");
 			System.out.println(" (2) Place Order");
 			System.out.println(" (3) ");
 			System.out.println(" (0) Back");
 			System.out.print("\n Choose: \n");
-			
+
 			int choice = intInput();
-			
-			switch (choice)
-			{
-				case 1:
-					createOffer();
-					break;
-				case 2:
-					createOrder();
-					break;
-				case 3:
-					System.out.println("Not yet possible");
-					break;
-				case 0:
-					running = false;
-					break;
-				default:
-					System.out.println("An error has happened, choice = " + choice);
-					break;
+
+			switch (choice) {
+			case 1:
+				createOffer();
+				break;
+			case 2:
+				createOrder();
+				break;
+			case 3:
+				System.out.println("Not yet possible");
+				break;
+			case 0:
+				running = false;
+				break;
+			default:
+				System.out.println("An error has happened, choice = " + choice);
+				break;
 			}
 		}
 	}
 
-	private void createOffer()
-	{
-		//TODO discuss whether the ctr is created here
-		boolean running = true;
-		
-		while(running) 
-		{
-			String customerName = findCustomer();
-			System.out.println("Creating offer for  " + customerName + ":");
-			
-			searchForProduct(); //creates a list of products and stores it in the OrderCtr
-			
-			displaySummary(customerName);
-			
-			confirm("offer");
-		}
+	private void createOffer() {
+		// TODO discuss whether the ctr is created here
+
+		String customerName = findCustomer();
+		System.out.println("Creating offer for  " + customerName + ":");
+
+		searchForProduct(); // creates a list of products and stores it in the OrderCtr
+
+		displaySummary(customerName);
+
+		confirm("offer");
 	}
 
-	public void createOrder()
-	{
-		boolean running = true;
-		
-		while(running)
-		{
-			String customerName = findCustomer();
-			System.out.println("Creating order for  " + customerName + ":");
-			
-			searchForProduct(); 
-			
-			displaySummary(customerName);
-			
-			confirm("order");
-		}	
+	public void createOrder() {
+
+		String customerName = findCustomer();
+		System.out.println("Creating order for  " + customerName + ":");
+
+		searchForProduct();
+
+		displaySummary(customerName);
+
+		confirm("order");
 	}
-		
-	
+
 	/**
-	 * This method returns the int we use to handle
-	 * user input errors when we are searching for
-	 * ints, and they input Strings instead.
+	 * This method returns the int we use to handle user input errors when we are
+	 * searching for ints, and they input Strings instead.
 	 * 
 	 * @return int
 	 */
-	private int intInput()
-	{
-
-		while (!input.hasNextInt())
-		{
+	private int intInput() {
+		while (!input.hasNextInt()) {
 			System.out.println("Input should be a number, try again");
 			input.nextLine();
 		}
@@ -113,40 +93,33 @@ public class OrderMenu
 		int choice = input.nextInt();
 		return choice;
 	}
-	
+
 	/**
-	 * This method allows us to search for customers
-	 * based on their phone numbers,
-	 * and return their name as a string, in order to use
-	 * it inside other methods.
+	 * This method allows us to search for customers based on their phone numbers,
+	 * and return their name as a string, in order to use it inside other methods.
 	 * 
 	 * @return the found customers name
 	 */
-	private String findCustomer() 
-	{
+	private String findCustomer() {
 		String customerName = null;
 		System.out.println("Please input customer's phone number: ");
 		int phone = intInput();
-		
-		if (Integer.toString(phone).length() != 8)
-		{
+
+		if(Integer.toString(phone).length() != 8) {
 			System.out.println("Phone number invalid, make sure not to use white spaces or country code.");
 			findCustomer();
 		}
-		
-		try 
-		{
+
+		try {
 			customerName = orderCtr.findCustomer(phone);
-		} 
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			customerName = findCustomer();
-			
+
 		}
 		return customerName;
 	}
-	
+
 	/**
 	 * This method allows us to search for products based
 	 * on their names and add them to the list
@@ -222,57 +195,63 @@ public class OrderMenu
 				productsAdded = true;
 			}
 		}
-	
+
 	/**
-	* 
-	*/
-	private void displaySummary(String customerName)
-	{
-			System.out.println("Summary:");
-			System.out.println("Customer: " + customerName);
-			System.out.println("Products: ");
-			
-			long priceWithoutDiscount = 0;
-			ArrayList<OrderLineItem> allProducts = orderCtr.getOrderProducts();
-			
-			for(OrderLineItem o : allProducts)
-			{
-				Product product = o.getProduct();
-				String name = product.getName();
-				long price = product.getSalesPrice();
-				int quantity = o.getQuantity();
-				long totalPrice = quantity * price; //Total price of the product based on quantity
-				priceWithoutDiscount += totalPrice;
-				
-				System.out.println(name + "				" + price + "kr.");
-				if(quantity > 1)
-				{
-					System.out.println(quantity +  " x " + price +				totalPrice + "kr.");
-				}
-				System.out.println("");
-				
+	 * This method shows a summary of whatever type of sale (orfer or order) is
+	 * currently stored in the order controller, by retrieving the list of
+	 * orderLineItems and the customer that has been saved. The parameter needed for
+	 * the method is the customer name, which should already have been identified,
+	 * prior to this method.
+	 */
+	private void displaySummary(String customerName) {
+		System.out.println("Summary:");
+		System.out.println("Customer: " + customerName);
+		System.out.println("Products: ");
+
+		long priceWithoutDiscount = 0;
+		ArrayList<OrderLineItem> allProducts = orderCtr.getOrderProducts();
+
+		for (OrderLineItem o : allProducts) {
+			Product product = o.getProduct();
+			String name = product.getName();
+			long price = product.getSalesPrice();
+			int quantity = o.getQuantity();
+			long totalPrice = quantity * price; // Total price of the product based on quantity
+			priceWithoutDiscount += totalPrice;
+
+			System.out.println(name + "				" + price + "kr.");
+			if (quantity > 1) {
+				System.out.println(quantity + " x " + price + totalPrice + "kr.");
 			}
-			
-			System.out.println("Price before discount " + priceWithoutDiscount + "kr.");
-			System.out.println("Total " + orderCtr.calculateTotal() + "kr.");
+			System.out.println("");
+
 		}
-		
-	private void confirm(String type)
-	{
-			System.out.println("\n Confirm " + type + "? Y/N");
-			String answer = input.nextLine(); 
-			if(answer.trim().equalsIgnoreCase("Y") || answer.trim().equalsIgnoreCase("yes"))
-			{
+
+		System.out.println("Price before discount " + priceWithoutDiscount + "kr.");
+		System.out.println("Total " + orderCtr.calculateTotal() + "kr.");
+	}
+
+	/**
+	 * This method is used to confirm an order based on the orderLineItems and
+	 * customer placed inside the OrderCtr. This is used to finalise the process of
+	 * creating an order or offer, and it sends the confirmation to the OrderCtr
+	 * that then creates the Order object and adds it to the OrderContainer
+	 */
+	private void confirm(String type) {
+		System.out.println("\n Confirm " + type + "? Y/N");
+		String answer = input.nextLine();
+		if (answer.trim().equalsIgnoreCase("Y") || answer.trim().equalsIgnoreCase("yes")) {
+			if (type.equals("offer")) {
 				orderCtr.createOffer();
-				System.out.println("The " + type + " has succesfully been created, you will now be returned to the Order Menu.");
-				running = false;
+			} else {
+				orderCtr.createOrder();
 			}
-			else
-			{
-				//TODO figure out if other things should happen if the offer is rejected.
-				System.out.println("The " + type + " was rejected, you will now be returned to the Order Menu.");
-				running = false;
-			}
+
+			System.out.println(
+					"The " + type + " has succesfully been created, you will now be returned to the Order Menu.");
+		} else {
+			// TODO figure out if other things should happen if the offer is rejected.
+			System.out.println("The " + type + " was rejected, you will now be returned to the Order Menu.");
 		}
-		
+	}
 }
