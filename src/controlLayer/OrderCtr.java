19e43e7b562f4ae616d5bd7febc11f29e5c142f8
Miroslav <1028757@ucn.dk>
2020-12-13 14:22:46 +0100

    OrderCtr - New method for calculating variable quantity

--- src/controlLayer/OrderCtr.java
+++ src/controlLayer/OrderCtr.java
@@ -15,21 +15,21 @@ public class OrderCtr
 	private Customer customer;
 	private ArrayList<OrderLineItem> orderProducts;
 	private ArrayList<Product> foundProducts;
-	
 
 	public OrderCtr()
 	{
+		orderProducts = new ArrayList<OrderLineItem>();
+		foundProducts = new ArrayList<Product>();
 	}
 
 	public ArrayList<OrderLineItem> getOrderProducts()
 	{
 		return orderProducts;
 	}
-	
+
 	/**
-	 * This method returns the customers name 
-	 * and also stores the customer Object in the customer field
-	 * of this class.
+	 * This method returns the customers name and also stores the customer
+	 * Object in the customer field of this class.
 	 * 
 	 * @param phone
 	 * @return String of customer name
@@ -39,7 +39,7 @@ public class OrderCtr
 		// TODO - check if correct
 		Customer customer = customerCtr.getCustomer(phone);
 		this.customer = customer;
-		if(customer == null)
+		if (customer == null)
 		{
 			throw new CustomerNotFoundException("No customer exists with this phone number: " + phone + "\n Make sure to write the phone number without the country code.");
 		}
@@ -47,42 +47,42 @@ public class OrderCtr
 	}
 
 	/**
-	 * This method calls the getProducts() in the product controller
-	 * and creates a list of products that are stored in the foundProducts
-	 * field. Then an Array of Strings containing info such
-	 * as the name an description of the product. This can then be used
-	 * in the UI layer.
+	 * This method calls the getProducts() in the product controller and creates
+	 * a list of products that are stored in the foundProducts field. Then an
+	 * Array of Strings containing info such as the name an description of the
+	 * product. This can then be used in the UI layer.
 	 * 
 	 * @param name
 	 * @return ArrayList of String Arrays containing product information
 	 */
-	 public ArrayList<String[]> getProducts(String name)
-	 {
+	public ArrayList<String[]> getProducts(String name)
+	{
 		foundProducts = productCtr.getProducts(name);
 		ArrayList<String[]> allProductsInfo = new ArrayList<String[]>();
-		
-		for(Product product: foundProducts)
+
+		for (Product product : foundProducts)
 		{
 			String[] productInfo = new String[2];
 			productInfo[0] = product.getName();
 			productInfo[1] = product.getDescription();
-			
-			allProductsInfo.add(productInfo);
+			//if the product exists in the cart already(one or multiple times), it substracts the ordered quantity and displays the quantity left
+			productInfo[2] = isProductInCart(product) > 0 ? Integer.toString(product.getQuantity() - isProductInCart(product)) : Integer.toString(product.getQuantity());
+			//if there is no quantity left, it does not display the product
+			if (product.getQuantity() - isProductInCart(product) > 0)
+			{
+				allProductsInfo.add(productInfo);
+			}
 		}
-		
+
 		return allProductsInfo;
-	 }
+	}
 
 	/**
-	 * This method allows us to have a product
-	 * selected from the foundProducts list,
-	 * and then based on the place in the list
-	 * add it to the orderProducts list
-	 * along with the quantity of the product
-	 * that is being ordered/offered. If the product
-	 * and quantity is successfully added to the orderProducts
-	 * list, the method returns true, if not
-	 * false.
+	 * This method allows us to have a product selected from the foundProducts
+	 * list, and then based on the place in the list add it to the orderProducts
+	 * list along with the quantity of the product that is being
+	 * ordered/offered. If the product and quantity is successfully added to the
+	 * orderProducts list, the method returns true, if not false.
 	 * 
 	 * @param placeInList
 	 * @param quantity
@@ -92,17 +92,15 @@ public class OrderCtr
 	{
 		Product product = foundProducts.get(placeInList);
 		OrderLineItem orderLineItem = new OrderLineItem(product, quantity);
-		
+
 		return orderProducts.add(orderLineItem);
 	}
 
 	/**
-	 *This method uses the fields customer and  
-	 *orderProducts to create
-	 *an order Object and add
-	 *it to our collection of orders.
-	 *It also creates an expiration date and automatically
-	 *sets status to pending. This is an offer because its status is set to pending.
+	 * This method uses the fields customer and orderProducts to create an order
+	 * Object and add it to our collection of orders. It also creates an
+	 * expiration date and automatically sets status to pending. This is an
+	 * offer because its status is set to pending.
 	 * 
 	 * @return true or false
 	 */
@@ -111,28 +109,25 @@ public class OrderCtr
 		Order offer = new Order(customer, orderProducts);
 		offer.calculateExpirationDate();
 		offer.setStatus("pending");
-		
+
 		/*
-		 * Here we update the threshold of the product, 
-		 * to make sure that the quantity in stock matches
-		 * the possible order size. The threshold however, should
-		 * be changed again, if the offer is not
-		 * accepted within the agreed expiration date
+		 * Here we update the threshold of the product, to make sure that the
+		 * quantity in stock matches the possible order size. The threshold
+		 * however, should be changed again, if the offer is not accepted within
+		 * the agreed expiration date
 		 */
-		for(OrderLineItem p: orderProducts)
+		for (OrderLineItem p : orderProducts)
 		{
 			p.getProduct().updateThreshold(p.getQuantity());
 		}
-		
+
 		return OrderContainer.getInstance().addOrder(offer);
 	}
 
 	/**
-	 * This method creates an Order,
-	 * by passing the customer and
-	 * the list of products and setting
-	 * the status of the order to confirmed
-	 * so indicate that it is an order.
+	 * This method creates an Order, by passing the customer and the list of
+	 * products and setting the status of the order to confirmed so indicate
+	 * that it is an order.
 	 * 
 	 * 
 	 */
@@ -140,18 +135,16 @@ public class OrderCtr
 	{
 		Order order = new Order(customer, orderProducts);
 		order.setStatus("confirmed");
-		
+
 		/*
-		 * Here we update the stock, by changing
-		 * the quantity of the product in stock
-		 * based on the quantity of the orderline
-		 * item. 
+		 * Here we update the stock, by changing the quantity of the product in
+		 * stock based on the quantity of the orderline item.
 		 */
-		for(OrderLineItem p: orderProducts)
+		for (OrderLineItem p : orderProducts)
 		{
 			p.getProduct().updateQuantity(p.getQuantity() * (-1));
 		}
-		
+
 		return OrderContainer.getInstance().addOrder(order);
 	}
 
@@ -164,16 +157,30 @@ public class OrderCtr
 	{
 		long totalWithoutDiscount = 0;
 		long totalWithDiscount = 0;
-		for(OrderLineItem p : orderProducts) 
+		for (OrderLineItem p : orderProducts)
 		{
 			Product product = p.getProduct();
 			int quantity = p.getQuantity();
 			totalWithoutDiscount += product.getSalesPrice() * quantity;
 			totalWithDiscount += product.getSalesPrice() * quantity * (100 - product.getDiscount(quantity));
 		}
-		
-		totalWithDiscount *= (100-customer.getDiscount());
-		
-		return totalWithDiscount/totalWithoutDiscount < 0.8 ? (int)0.8 * totalWithoutDiscount : totalWithDiscount;
+
+		totalWithDiscount *= (100 - customer.getDiscount());
+
+		return totalWithDiscount / totalWithoutDiscount < 0.8 ? (int)0.8 * totalWithoutDiscount : totalWithDiscount;
 	}
+
+	private int isProductInCart(Product product)
+	{
+		int tempQuantity = 0;
+		for (OrderLineItem oli : orderProducts)
+		{
+			if (oli.getProduct().getBarcode().equals(product.getBarcode()))
+			{
+				tempQuantity += oli.getQuantity();
+			}
+		}
+		return tempQuantity;
+	}
+
 }
