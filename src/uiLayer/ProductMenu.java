68580828fe5096037029e39e5e5a727071bf4624
TMSKHL <2000tamas@gmail.com>
2020-12-13 17:58:52 +0100

    ProductMenu update
    Created the createProduct, readProduct and updateProduct functions

--- src/uiLayer/ProductMenu.java
+++ src/uiLayer/ProductMenu.java
@@ -1,6 +1,7 @@
 package uiLayer;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import controlLayer.*;
@@ -57,17 +58,164 @@ public class ProductMenu {
 	
 	private void createProduct()
 	{
-		//TODO write this
+		//TODO check if user communication has a good form
+		int threshold,quantity,purchasePrice,salesPrice,discount;
+		String barcode,name,description,group,location;
+		//********************** Get name **********************\\
+		System.out.println("Please enter the name of the product");
+		name = input.nextLine();
+		while (name.length() < 3)
+		{
+			System.out.println("The name should we longer than 2 characters! Please try an another one!");
+			name = input.nextLine();
+		}
+		
+		//********************** Get description **********************\\
+		System.out.println("Please enter the description of the product");
+		description = input.nextLine();
+		while (description.length() == 0)
+		{
+			System.out.println("The desciption shouldnt be empty! Please try an another one!");
+			description = input.nextLine();
+		}
+		
+		//********************** Get group **********************\\
+		System.out.println("Please enter the group of the product");
+		group = input.nextLine();
+		while (group.length() == 0)
+		{
+			System.out.println("The group shouldnt be empty! Please try an another one!");
+			group = input.nextLine();
+		}
+		
+		//********************** Get barcode **********************\\
+		System.out.println("Please enter the barcode of the product");
+		barcode = input.nextLine();
+		while (barcode.length() == 13)
+		{
+			System.out.println("The barcode has to be 13 characters! Please try an another one!");
+			barcode = input.nextLine();
+		}
+		
+		//********************** Get location **********************\\
+		System.out.println("Please enter the location of the product");
+		location = input.nextLine();
+		while (location.length() == 0)
+		{
+			System.out.println("The location shouldnt be empty! Please try an another one!");
+			location = input.nextLine();
+		}
+		
+		
+		//********************** Get quantity **********************\\
+		System.out.println("Please enter the quantity of the product");
+		quantity = intInput();
+		while (quantity == 0)
+		{
+			System.out.println("The quantity shouldn't zero! Please try an another one!");
+			quantity = intInput();
+		}
+		
+		
+		//********************** Get threshold **********************\\
+		System.out.println("Please enter the threshold of the product");
+		threshold = intInput();
+		while (threshold == 0)
+		{
+			System.out.println("The threshold shouldn't zero! Please try an another one!");
+			threshold = intInput();
+		}		
+
+		
+		//********************** Get salesPrice **********************\\
+		System.out.println("Please enter the saleprice of the product");
+		salesPrice = intInput();
+		while (salesPrice == 0)
+		{
+			System.out.println("The salesPrice shouldn't zero! Please try an another one!");
+			salesPrice = intInput();
+		}
+		
+		
+		//********************** Get purchasePrice **********************\\
+		System.out.println("Please enter the purchaseprice of the product");
+		purchasePrice = intInput();
+		while (purchasePrice == 0)
+		{
+			System.out.println("The purchasePrice shouldn't zero! Please try an another one!");
+			purchasePrice = intInput();
+		}
+		
+		
+		//********************** Get discount **********************\\
+		System.out.println("Please enter the discount of the product");
+		discount = intInput();
+		while (discount < 0 || discount > 100)
+		{
+			System.out.println("Invalid discount! Please try an another one!");
+			discount = intInput();
+		}
+		//
+		
+		if (productCtr.createProduct(threshold,quantity,purchasePrice,salesPrice,barcode,name,description,group,location))
+		{
+			System.out.println("Product creation successful!");
+		}
+		else
+		{
+			System.out.println("Product creation error!");
+		}
+		
 	}
 	
 	private void readProduct()
 	{
-		//TODO write this
+		int choice = getProduct();	
+		productCtr.getInfo(choice);
 	}
 	
 	private void updateProduct()
 	{
-		//TODO write this
+		int choice = getProduct();
+		productCtr.getParameters(choice);
+		
+		boolean running = true;
+		int element;
+		while (running)
+		{
+			System.out.println("Please choose an element you want to edit");
+			element = intInput();
+			while (element <= 0 || element > 10)
+			{
+				System.out.println("Please choose a number from the list!");
+				element = intInput();
+			}
+			productCtr.updateParameter(element);
+			System.out.println("If you want to edit an another parameter of the product type'y' or 'yes' otherwise type 'n' or 'no' ");
+			String answer = "run";
+			while (answer.equals("run"))
+			{
+				answer = input.nextLine();
+				if (answer.trim().equalsIgnoreCase("N") || answer.trim().equalsIgnoreCase("no"))
+				{
+					running = false;
+					answer = "notRun";
+					System.out.println("Returning to the menu:");
+				}
+				
+				else if (answer.trim().equalsIgnoreCase("Y") || answer.trim().equalsIgnoreCase("yes"))
+				{
+					answer = "notRun";
+				}
+				
+				else
+				{
+					System.out.println("Unknown command!");
+				}
+			}
+		}
+		
+		
 	}
 	
 	private void deleteProduct()
@@ -84,6 +232,36 @@ public class ProductMenu {
 		}
 
 		int choice = input.nextInt();
+		return choice;
+	}
+	
+	private int getProduct()
+	{
+		System.out.println("Please enter the name of the product:");
+		String name = input.nextLine();
+		while (name.length() < 3)
+		{
+			System.out.println("Please enter a name with more than 2 characters!");
+			name = input.nextLine();
+		}
+		ArrayList<String[]> data = readProduct(name);
+		System.out.println("Please select an product from the list:");
+		int place = 0;
+		for (String[] element : data)
+		{
+			System.out.println(""+(++place)+")");
+			System.out.println("Name of the product: "+element[0] );
+			System.out.println("Description: "+element[1] );
+			
+		}
+		System.out.println("Number: ");
+		int choice = input.nextInt();
+		while (choice <= 0 || choice > place)
+		{
+			System.out.println("Please enter a number between 1 and "+place+"!");
+			choice = input.nextInt();
+		}
+		
 		return choice;
 	}
 }
