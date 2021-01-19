08acadc29540f66d8b4b331859e3b9a75f3f66a9
martinko-boop <75024378+martinko-boop@users.noreply.github.com>
2021-01-19 10:30:37 +0100

    Update EditCustomerDialog.java

--- src/guiLayer/EditCustomerDialog.java
+++ src/guiLayer/EditCustomerDialog.java
@@ -9,6 +9,7 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
@@ -89,16 +90,16 @@ public class EditCustomerDialog extends JDialog {
 		contentPanel.setBackground(new Color(252, 252, 252));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_contentPanel = new GridBagLayout();
-		gbl_contentPanel.columnWidths = new int[]{0, 0};
+		gbl_contentPanel.columnWidths = new int[]{0, 0, 0};
 		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0};
-		gbl_contentPanel.columnWeights = new double[]{1.0, 0.0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, 0.0};
 		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
 		contentPanel.setLayout(gbl_contentPanel);
 		{
 			header = new JPanel();
 			header.setBackground(babyBlue);
 			GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-			gbc_panel_1.gridwidth = 2;
+			gbc_panel_1.gridwidth = 3;
 			gbc_panel_1.insets = new Insets(0, 0, 5, 0);
 			gbc_panel_1.fill = GridBagConstraints.BOTH;
 			gbc_panel_1.gridx = 0;
@@ -115,9 +116,10 @@ public class EditCustomerDialog extends JDialog {
 		}
 		{
 			customerName = new JLabel("<CustomerNamePlaceHolder>");
+			customerName.setHorizontalAlignment(SwingConstants.LEFT);
 			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 			gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
-			gbc_lblNewLabel.insets = new Insets(5, 50, 5, 5);
+			gbc_lblNewLabel.insets = new Insets(5, 5, 5, 5);
 			gbc_lblNewLabel.gridx = 0;
 			gbc_lblNewLabel.gridy = 1;
 			contentPanel.add(customerName, gbc_lblNewLabel);
@@ -127,7 +129,7 @@ public class EditCustomerDialog extends JDialog {
 			panel.setBorder(BorderFactory.createLineBorder(new Color(243, 243, 243), 2, true));
 			panel.setBackground(Color.WHITE);
 			GridBagConstraints gbc_panel = new GridBagConstraints();
-			gbc_panel.gridwidth = 2;
+			gbc_panel.gridwidth = 3;
 			gbc_panel.insets = new Insets(20, 50, 20, 50);
 			gbc_panel.fill = GridBagConstraints.BOTH;
 			gbc_panel.gridx = 0;
@@ -135,7 +137,7 @@ public class EditCustomerDialog extends JDialog {
 			contentPanel.add(panel, gbc_panel);
 			GridBagLayout gbl_panel = new GridBagLayout();
 			gbl_panel.columnWidths = new int[]{0};
-			gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+			gbl_panel.rowHeights = new int[]{20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 			gbl_panel.columnWeights = new double[]{1.0};
 			gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 			panel.setLayout(gbl_panel);
@@ -256,13 +258,24 @@ public class EditCustomerDialog extends JDialog {
 			}
 		}
 		{
+			RoundedButton deleteButton = new RoundedButton("Delete", Color.RED, Color.WHITE, Color.RED, new Font("Lato", Font.BOLD, 15));
+			formatButton(deleteButton);
+			redButton(deleteButton);
+			GridBagConstraints gbc_deleteButton = new GridBagConstraints();
+			gbc_deleteButton.anchor = GridBagConstraints.WEST;
+			gbc_deleteButton.insets = new Insets(0, 5, 5, 0);
+			gbc_deleteButton.gridx = 0;
+			gbc_deleteButton.gridy = 3;
+			contentPanel.add(deleteButton, gbc_deleteButton);
+		}
+		{
 			RoundedButton okButton = new RoundedButton("Finish", new Color(28, 150, 202), Color.WHITE, babyBlue, new Font("Lato", Font.BOLD, 15));
 			formatButton(okButton);
 			blueButton(okButton);
 			GridBagConstraints gbc_okButton = new GridBagConstraints();
 			gbc_okButton.anchor = GridBagConstraints.SOUTHEAST;
 			gbc_okButton.insets = new Insets(0, 0, 5, 5);
-			gbc_okButton.gridx = 0;
+			gbc_okButton.gridx = 1;
 			gbc_okButton.gridy = 3;
 			contentPanel.add(okButton, gbc_okButton);
 		}
@@ -273,7 +286,7 @@ public class EditCustomerDialog extends JDialog {
 			GridBagConstraints gbc_cancelButton = new GridBagConstraints();
 			gbc_cancelButton.anchor = GridBagConstraints.SOUTHEAST;
 			gbc_cancelButton.insets = new Insets(0, 0, 5, 5);
-			gbc_cancelButton.gridx = 1;
+			gbc_cancelButton.gridx = 2;
 			gbc_cancelButton.gridy = 3;
 			contentPanel.add(cancelButton, gbc_cancelButton);
 		}
@@ -299,6 +312,35 @@ public class EditCustomerDialog extends JDialog {
 			{
 				button.setBackground(babyBlue);
 				button.setForeground(Color.WHITE);
+			}
+		});
+	}
+	
+	private void redButton(RoundedButton button) {
+		button.addMouseListener(new MouseAdapter()
+		{	
+			@Override
+			public void mouseEntered(MouseEvent e) 
+			{
+				button.setBackground(Color.WHITE);
+				button.setForeground(Color.RED);
+			}
+					
+			@Override
+			public void mouseExited(MouseEvent e) 
+			{
+				button.setBackground(Color.RED);
+				button.setForeground(Color.WHITE);
+			}
+			
+			public void mouseClicked(MouseEvent e)
+			{
+				int confirmation = JOptionPane.showConfirmDialog(null, "Confirm deletion", "Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+				if(confirmation == JOptionPane.YES_OPTION) 
+				{
+					//TODO Delete Customer
+					dispose();
+				}
 			}
 		});
 	}
