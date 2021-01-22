6c36c2bbd3e5b74912e5a9821462644324fcb065
miroslavmarinov <miroslavmarinov@miroslavs-macbook-pro-2.home>
2021-01-22 13:14:30 +0100

    Custom JTable Button renderer

--- src/guiLayer/Renderers/JTableButtonRenderer.java
+++ src/guiLayer/Renderers/JTableButtonRenderer.java
@@ -11,26 +11,33 @@ import javax.swing.table.TableCellRenderer;
 
 import guiLayer.RoundedButton;
 
-public class JTableButtonRenderer implements TableCellRenderer {
+/**
+ * Custom implementation of TableCellRenderer for retrieving the right content from a button cell.
+ * @author Group1
+ *
+ */
+public class JTableButtonRenderer implements TableCellRenderer
+{
 	
-	   private TableCellRenderer defaultRenderer;
+   private TableCellRenderer defaultRenderer;
+   
+   public JTableButtonRenderer(TableCellRenderer renderer)
+   {
+      defaultRenderer = renderer;
+   }
+   
+   /**
+    * Custom method for overriding the superclass' method for retrieving the TableCellRendererComponent(Table cell content) for a button.
+    * @return returns the content of a cell wrapped in a Component object.
+    */
+   @Override
+   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+   {
 	   
-	   public JTableButtonRenderer(TableCellRenderer renderer) {
-	      defaultRenderer = renderer;
-	   }
-	   
-	   @Override
-	   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-		   
-		 /*  if(value instanceof RoundedButton) {
-			   RoundedButton button = (RoundedButton) value;
-			   button.setForeground(Color.WHITE);
-		   }
-		   */
-		   
-	      if(value instanceof Component) {
-	    	  return (Component)value;
-	      }
-	         return defaultRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-	   }
-	}
+      if(value instanceof Component)
+      {
+    	  return (Component)value;
+      }
+         return defaultRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+   }
+}
