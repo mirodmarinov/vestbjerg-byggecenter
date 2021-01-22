9d992a923c215e98d58334ff1bb5afecf21f3875
miroslavmarinov <miroslavmarinov@miroslavs-macbook-pro-2.home>
2021-01-22 13:19:29 +0100

    OrderTableRenderer comments added

--- src/guiLayer/Renderers/OrderTableRenderer.java
+++ src/guiLayer/Renderers/OrderTableRenderer.java
@@ -7,16 +7,24 @@ import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
+/**
+ *  Custom TableCellRenderer that replaces the
+ *  Component of each cell with an extended JLabel Object.
+ *  This makes most cells act and have the functionality of a Jlabel.
+ * @author group1
+ *
+ */
 public class OrderTableRenderer extends JLabel implements TableCellRenderer{
 	
 	    public OrderTableRenderer() {
 	        setOpaque(true);
 	    }
-	     
+	     /**
+	      * Returns a custom component that is a JLabel
+	      */
 	    @Override
 	    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
 	        boolean hasFocus, int row, int column) {
-	    	//this.setText(value.toString());
 	        this.setAlignmentX(LEFT_ALIGNMENT);
 	        this.setBackground(Color.WHITE);
 	        return this;
